839b831623a2fb18a1d0775c7a68937979aed543
totsuchida <toshiki.tsuchida.dev@gmail.com>
2021-08-20 01:41:28 +0900

    connet run to sql

--- terraform/cloudsql.tf
+++ terraform/cloudsql.tf
@@ -1,9 +1,16 @@
-resource "google_sql_database_instance" "cloud-sql-test" {
-  name             = "cloud-sql-test"
+resource "google_sql_database" "cloud-sql-test-db" {
+  name     = "cloud-sql-test-db"
+  instance = google_sql_database_instance.cloud-sql-test-instance.name
+}
+
+resource "google_sql_database_instance" "cloud-sql-test-instance" {
+  name             = "cloud-sql-test-instance"
   database_version = "MYSQL_8_0"
   region           = var.DEFAULT_REGION
 
   settings {
-    tier = "db-f1-micro"
+    tier              = "db-f1-micro"
+    availability_type = "ZONAL"
+    disk_size         = 1
   }
 }
--- terraform/service-account.tf
+++ terraform/service-account.tf
@@ -6,3 +6,7 @@ resource "google_project_iam_member" "cloud-run-agent-iam" {
   role   = "roles/run.serviceAgent"
   member = "serviceAccount:${google_service_account.cloud-run-sa.email}"
 }
+resource "google_project_iam_member" "sql-for-cloud-run-iam" {
+  role   = "roles/cloudsql.client"
+  member = "serviceAccount:${google_service_account.cloud-run-sa.email}"
+}
